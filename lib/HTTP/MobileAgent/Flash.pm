812eeedfb60134067876c10d106d3bd7862ef88a
takefumi <takefumi@d0d07461-0603-4401-acd4-de1884942a52>
2009-11-16 05:28:27 +0000

    lang/perl/HTTP-MobileAgent-Flash: release version '0.08'.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAgent-Flash/trunk@35923 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAgent/Flash.pm
+++ lib/HTTP/MobileAgent/Flash.pm
@@ -2,7 +2,7 @@ package HTTP::MobileAgent::Flash;
 use strict;
 use warnings;
 use vars qw($VERSION);
-$VERSION = '0.07';
+$VERSION = '0.08';
 
 use HTTP::MobileAgent;
 use HTTP::MobileAgent::Flash::DoCoMoFlashMap;
